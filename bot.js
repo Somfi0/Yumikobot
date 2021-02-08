0195684e8b5e07a5fb3afb4eb42b9e386dcd34d2
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-08 10:38:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const comms = require("./commands.js");
 
 client.on("ready", function () {
         console.log(` Господин я ${client.user.tag} включилась! `);
-        client.user.setStatus("online")
+        client.user.setStatus("dnd")
         client.user.setPresence({
           status: "dnd",
           activity: {
