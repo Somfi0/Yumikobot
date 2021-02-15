b3cfac10de775ca8dbec791d7b76daa4fd74204c
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-15 20:26:48 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,1 +1,9 @@
-
+module.exports = client => {
+  client.on('guildMemberAdd', member => {
+    
+    const messwelc = `Please welcome <@${member.id}> to the Yumiko`
+    
+    const channel = member.guild.channels.cache.get(channelId)
+    channel.send(messwelc)
+  })
+}
