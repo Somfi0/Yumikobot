b106031aadffa80496aebb0af9ea8ee77368fa59
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 12:34:10 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -3,13 +3,13 @@ module.exports = client => {
 client.on('guildMemberAdd', member => {
     
 
-    const welcomeEmbed = new Discord.MessageEmbed()
-
-    welcomeEmbed.setColor("#f5ec42")
-    welcomeEmbed.setTitle(`Приветствую тебя <@${member.id}> на нашем сервере Yumiko`)
-    welcomeEmbed.setImage("https://media.discordapp.net/attachments/751078000430284912/805450212298326016/image_860911170005395524955.gif")
-
     const channel = member.guild.channels.cache.get(channelId)
-    channel.send(welcomeEmbed)
+    channel.send({embed: {
+    color: ("#f5ec42"),
+    title: "Приветствие",
+    description: `Приветствую тебя <@${member.id}> на нашем сервере Yumiko`,
+    "image": {
+    "url": "https://media.discordapp.net/attachments/751078000430284912/805450212298326016/image_860911170005395524955.gif"
+    }})
 })
 }
