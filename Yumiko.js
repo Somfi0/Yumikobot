e25560453f205e2bde413fcef8b927e96373a567
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 12:45:11 +0200

    Update Yumiko.js

--- Yumiko.js
+++ Yumiko.js
@@ -22,19 +22,6 @@ client.on("ready", function () {
         })
         
     });
-client.on('guildMemberAdd', member => {
-    const channelId = '810941652004503563'
-
-    const channel = member.guild.channels.cache.get(channelId)
-    channel.send({embed: {
-    color: ("#f5ec42"),
-    title: "Приветствие",
-    description: `Приветствую тебя <@${member.id}> на нашем сервере Yumiko`,
-    "image": {
-    "url": "https://media.discordapp.net/attachments/751078000430284912/805450212298326016/image_860911170005395524955.gif"
-    }
-  })
-});
 
     client.on('message', (msg) => { // Реагирование на сообщения
         if (msg.author.username != client.user.username && msg.author.discriminator != client.user.discriminator) {
@@ -49,5 +36,17 @@ client.on('guildMemberAdd', member => {
           }
         }
       });
+client.on('guildMemberAdd', member => {
+    const channelId = '810941652004503563'
 
+    const channel = member.guild.channels.cache.get(channelId)
+    channel.send({embed: {
+    color: ("#f5ec42"),
+    title: "Приветствие",
+    description: `Приветствую тебя <@${member.id}> на нашем сервере Yumiko`,
+    "image": {
+    "url": "https://media.discordapp.net/attachments/751078000430284912/805450212298326016/image_860911170005395524955.gif"
+    }
+  })
+});
     client.login(token);
