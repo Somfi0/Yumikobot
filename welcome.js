7b687ebb4086667ab144c21217d6b2a8774417ac
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 11:57:32 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -2,13 +2,10 @@ module.exports = client => {
     const channelId = '810941652004503563'
     
 client.on('guildMemberAdd', (member) => {
-    console.log.(member)
+    console.log(member)
   
-    const welcomemess = new Discord.MessageEmbed()
-        welcomemess.setColor('#5cf000')
-    welcomemess.setTitle('**' + member.user.username + '** is now Among Us other **' + member.guild.memberCount + '** people')
-    welcomemess.setImage('https://cdn.mos.cms.futurecdn.net/93GAa4wm3z4HbenzLbxWeQ-650-80.jpg.webp')
-
-    member.guild.channels.cache.find(i => i.name === channelId).send(welcomemess)
+    const welcomemess = `Приветствую тебя <@${member.id}> на нашем сервере Yumiko`
+    const channel = member.guild.channels.cache.get(channelId)
+    channel.send(message)
 })
 }
