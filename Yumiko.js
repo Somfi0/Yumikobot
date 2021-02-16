bb9520de839802f43e3b38f82436c8ef4d48ade4
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 12:46:54 +0200

    Update Yumiko.js

--- Yumiko.js
+++ Yumiko.js
@@ -36,18 +36,6 @@ client.on("ready", function () {
           }
         }
       });
-client.on('guildMemberAdd', member => {
-    const channelId = '810941652004503563'
 
-    const channel = member.guild.channels.cache.get(channelId)
-    channel.send({embed: {
-    color: ("#f5ec42"),
-    title: "Приветствие",
-    description: `Приветствую тебя <@${member.id}> на нашем сервере Yumiko`,
-    "image": {
-    "url": "https://media.discordapp.net/attachments/751078000430284912/805450212298326016/image_860911170005395524955.gif"
-    }
-  })
-});
         
     client.login(token);
