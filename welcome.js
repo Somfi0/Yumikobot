2804a957fa6fb56d72e98c1aa7ea3ca4a893c664
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 13:34:58 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js'); 
+module.exports = client => {
  const channelId = '810941652004503563'
     
 client.on('guildMemberAdd', (member) => {
     console.log(member)
 
       const welcomemsg = new Discord.MessageEmbed()
-      .setColor('#0099ff')
-	.setTitle('Some title')
-	.setDescription('Some description here')
+      welcomemsg.setColor('#0099ff')
+	welcomemsg.setTitle('Some title')
+	welcomemsg.setDescription('Some description here')
     
     const channel = member.guild.channels.cache.get(channelId)
     channel.send(welcomemsg)
 })
-
+}
