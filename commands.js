560262c3f2124f0266ec4aad8a9a60fb0b304118
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 20:47:27 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -9,8 +9,9 @@ const { guild } = mess
 
 const {name, memberCount, owner} = guild
 const icon = guild.iconURL()
- function checkDays(date) {
+function checkDays(date) {
         let now = new Date();
+        let diff = now.getTime() - date.getTime();
         let days = Math.floor(diff / 86400000);
         return days + (days == 1 ? " day" : " days") + " ago";
     };
