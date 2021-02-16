b752cc547f535742bb9b75b525409a161aeabf4e
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 11:07:48 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,1 +1,32 @@
-
+module.exports = client =>{
+client.on('guildMemberAdd', msg => { // Commands Go Inside The client.on('message', 
+msg => )
+msg.guild.channels.get('810941652004503563').send({embed: {
+color: 3447003,
+author: {
+  name: client.user.username,
+  icon_url: client.user.avatarURL
+},
+title: "Welcome To ()!",
+url: "https://districtservices.net",
+description: "@MEMBER",
+fields: [{
+    name: "Fields",
+    value: "They can have different fields with small headlines."
+  },
+  {
+    name: "Masked links",
+    value: "You can put [masked links](http://google.com) inside of rich embeds."
+  },
+  {
+    name: "Markdown",
+    value: "You can put all the *usual* **__Markdown__** inside of them."
+  }
+],
+timestamp: new Date(),
+footer: {
+  icon_url: client.user.avatarURL,
+  text: "© Example"
+}
+}}); });
+}
