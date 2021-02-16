d8e0d402928fd3d412b78f1da4e6ade0b75f83fb
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 12:58:57 +0200

    Update Yumiko.js

--- Yumiko.js
+++ Yumiko.js
@@ -5,9 +5,12 @@ let config = require('./config.json'); // Подключаем файл с па�
 let token = config.token; // «Вытаскиваем» из него токен
 let prefix = config.prefix; // «Вытаскиваем» из него префикс
 const comms = require("./commands.js");
+const welcome = require("./welcome.js");
 
 client.on("ready", function () {
         console.log(` Господин я ${client.user.tag} включилась!! `)
+        
+        welcome(client)
         
         client.user.setStatus("dnd")
         client.user.setPresence({
