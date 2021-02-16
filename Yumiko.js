0aac21c3aa369663c64d62bc7505bfa54bcbdb6e
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 16:54:43 +0200

    Update Yumiko.js

--- Yumiko.js
+++ Yumiko.js
@@ -21,7 +21,7 @@ client.on("ready", function () {
     });
 
 client.on('guildMemberAdd', (member) => {
-    let chanellId = '786864575797919795';
+    let chanellId = '811247441666899968';
     let rules = '786897035457724436';
     let chat = '786858058323722241';
     let color = '805885775764783154';
