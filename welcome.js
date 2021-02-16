85877da67cf2e2bfb8d094bef7084387d485c251
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 13:29:54 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,13 +1,16 @@
+
 module.exports = client => {
  const channelId = '810941652004503563'
     
 client.on('guildMemberAdd', (member) => {
     console.log(member)
 
-      
-    const message = `Приветствую тебя <@${member.id}> на нашем сервере Yumiko`
+      const welcomemsg = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+	.setTitle('Some title')
+	.setDescription('Some description here')
     
     const channel = member.guild.channels.cache.get(channelId)
-    channel.send(message)
+    channel.send(welcomemsg)
 })
 }
