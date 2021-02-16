0113b2e71b1e0b6766aa0af8c97f01f86c6ab30d
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 10:55:17 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,10 +1,27 @@
 module.exports = (client) => {
-  const channelId = '810941652004503563'
-  client.on('guildMemberAdd', member => {
-    
-    const messwelc = `Please welcome <@${member.id}> to the Yumiko`
-    
-    const channel = member.guild.channels.cache.get(channelId)
-    channel.send(messwelc)
+client.on('guildMemberAdd', member => {
+client.on('message', 
+
+
+var role = member.guild.roles.find('Yumiko', 'Yumiko'); // Variable to get channel ID
+member.addRole(role); // Adds the default role to members
+
+member.guild.channels.get('810941652004503563').send({embed: {
+color: 3447003,
+title: "**SERVER NAME** Welcome Bot!",
+url: "WEBSITE URL",
+description: "Welcome *" + member + "* to the **Server name** discord server!",
+fields: [{
+    name: "Information",
+    value: "Some info on the server"
+  }
+],
+timestamp: new Date(),
+footer: {
+  icon_url: client.user.avatarURL,
+  text: "© NAME OF SERVER 2018 - 2019"
+}
+}}); });
   })
 }
+
