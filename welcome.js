d275dec461c8a6793f01ab521618724df15a9ebb
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-02-16 13:32:15 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,5 +1,4 @@
-
-module.exports = client => {
+const Discord = require('discord.js'); 
  const channelId = '810941652004503563'
     
 client.on('guildMemberAdd', (member) => {
@@ -13,4 +12,4 @@ client.on('guildMemberAdd', (member) => {
     const channel = member.guild.channels.cache.get(channelId)
     channel.send(welcomemsg)
 })
-}
+
